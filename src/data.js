c8412a83e510241c7c3552d72201118dbff60380
NataRuto <13natik@mail.ru>
2022-12-21 23:00:11 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id: 1,
+        gift: "Chocolate",
+        image: "https://images.unsplash.com/photo-1481391319762-47dff72954d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNob2NvbGF0ZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    }, 
+
+    {
+        id: 2,
+        gift: "Mascara",
+        image: "https://images.unsplash.com/photo-1623071280399-238e1f181fa6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG1hc2NhcmF8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+    }, 
+
+    {
+        id: 3,
+        gift: "Pandora",
+        image: "https://images.unsplash.com/photo-1608995840575-3657c3898373?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8UGFuZG9yYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    }, 
+
+    {
+        id: 4,
+        gift: "Dress",
+        image: "https://images.unsplash.com/photo-1539008835657-9e8e9680c956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZHJlc3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+    }, 
+
+    {
+        id: 5,
+        gift: "Travel to Paris",
+        image: "https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fFRyYXZlbCUyMHRvJTIwUGFyaXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+    }, 
+
+    {
+        id: 6,
+        gift: "Car",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQpiidpcUntLArvB39cQWdE8dvP2vYxCSJDg&usqp=CAU"
+    }, 
+]
